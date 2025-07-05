80c1fd700acccd51d66b482fa4e9cad2e66464dc
Chatree Benjasinpradit <pingsovo@gmail.com>
2025-07-05 21:51:03 +0700

    Refactor: Revert Tailwind CSS to use @tailwindcss/vite plugin

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,15 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
+
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     react(),
+    tailwindcss(),
   ],
   server: {
     host: '0.0.0.0', // เพิ่มบรรทัดนี้
     port: 3000,      // (ไม่บังคับ) ตรวจสอบให้แน่ใจว่าเป็นพอร์ตเดียวกับที่คุณแมปใน docker-compose.yml
-  },
-  css: {
-    postcss: './postcss.config.cjs',
   },
 })
